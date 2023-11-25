63fb597b6a2304ee9c6cc8042521b79952f0b5b6
n622 <jwithelis@gmail.com>
2023-11-25 21:21:12 +0900

    succesCatchKeyInput

--- src/ign_template/src/teleop_node.cpp
+++ src/ign_template/src/teleop_node.cpp
@@ -3,12 +3,19 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "rclcpp_components/register_node_macro.hpp"
+#include "key_event_nodes/key_hit_event_node.hpp"
+#include "key_event_msgs/msg/key_event.hpp"
+
 
 using namespace std::chrono_literals;
 
 class TeleopNode : public rclcpp::Node {
 public:
-    TeleopNode() : Node("teleop_node") {
+    TeleopNode(
+        const std::string& name_space="", 
+        const rclcpp::NodeOptions& options = rclcpp::NodeOptions()
+    ) : Node("teleop_node",name_space,options) {
         
         publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("teleop", 10);
 
@@ -26,15 +33,31 @@ public:
 
         subscription_ = this->create_subscription<std_msgs::msg::String>
                 ("feedBack_topic", 10, topic_callback);
+
+        sub_ = this->create_subscription<key_event_msgs::msg::KeyEvent>(
+        "key_hit_event",
+        rclcpp::QoS(10),
+        [this](const key_event_msgs::msg::KeyEvent::SharedPtr msg) ->void {
+            RCLCPP_INFO(this->get_logger(), "subscriber:key:%c", msg->key);
+        }
+        );
     }
 private:
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+    rclcpp::Subscription<key_event_msgs::msg::KeyEvent>::SharedPtr sub_;
 };
 
 int main(int argc, char *argv[]) {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<TeleopNode>());
+
+    rclcpp::executors::SingleThreadedExecutor exec;
+    auto node1 = std::make_shared<key_event::KeyHitEventNode>();
+    exec.add_node(node1);
+    auto node2 = std::make_shared<TeleopNode>();
+    exec.add_node(node2);
+    exec.spin();
+
     rclcpp::shutdown();
     return 0;
 }
